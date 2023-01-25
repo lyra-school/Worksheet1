b7d350b9c42a74d77c4c18f31d221926f66b034c
lyra-school <s00233718@atu.ie>
2023-01-25 10:23:57 +0000

    changes

---
+++ Q2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Name: Lyra Karsaj
+ * Date: 25/1/23
+ * Desc: Number counter + average
+ */
+namespace Q2
+{
+    internal class Program
+    {
+        //a
+        private static void CountOddNumbers()
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
--- Worksheet1/Program.cs
+++ Worksheet1/Program.cs
@@ -1,10 +1,65 @@
-﻿namespace Worksheet1
+﻿/* Name: Lyra Karsaj
+ * Date: 25/1/23
+ * Desc: Looping methods
+ */
+namespace Worksheet1
 {
     internal class Program
     {
+        //a
+        private static void PrintIntegersFor()
+        {
+            for(int i = 40; i < 61; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        //b
+        private static void PrintIntegersWhile()
+        {
+            int i = 40;
+            while(i < 61)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+        //c
+        private static void PrintIntegersDo()
+        {
+            int i = 40;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i < 61);
+        }
+        //d
+        private static void PrintExcludingFor()
+        {
+            for(int i = 40; i < 61; i++)
+            {
+                if(i != 46 && i != 48)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        //e
+        private static void PrintDescendingFor()
+        {
+            for (int i = 60; i > 39; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PrintIntegersFor();
+            PrintIntegersWhile();
+            PrintIntegersDo();
+            PrintExcludingFor();
+            PrintDescendingFor();
         }
     }
 }
