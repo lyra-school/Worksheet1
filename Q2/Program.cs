9bc440296446edca3dc12fbd3efd37ef8490a9b0
lyra-school <s00233718@atu.ie>
2023-01-25 10:44:05 +0000

    changes

--- Q2/Program.cs
+++ Q2/Program.cs
@@ -7,13 +7,59 @@ namespace Q2
     internal class Program
     {
         //a
-        private static void CountOddNumbers()
+        private static int CountOddNumbers()
         {
-            
+            List<int> numbers = new List<int>();
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter an integer: ");
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+            int counter = 0;
+            foreach(int i in numbers)
+            {
+                if(i % 2 == 1)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        //b
+        private static void AverageMinMax()
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter an integer: ");
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+            double average = 0;
+            int largestNumber = numbers[0];
+            int smallestNumber = numbers[0];
+            foreach (int i in numbers)
+            {
+                if (i > largestNumber)
+                {
+                    largestNumber = i;
+                }
+                if(i < smallestNumber)
+                {
+                    smallestNumber = i;
+                }
+                average = average + i;
+            }
+            average = average / numbers.Count;
+            Console.WriteLine(average);
+            Console.WriteLine(smallestNumber);
+            Console.WriteLine(largestNumber);
+
         }
         static void Main(string[] args)
         {
-            
+            int oddNumberCount = CountOddNumbers();
+            Console.WriteLine(oddNumberCount);
+            AverageMinMax();
         }
     }
 }
